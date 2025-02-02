620a4347d49a8e801083ed39112f5b9c6cc6c790
Tae-ho Kim <HoTaeWang@gmail.com>
2025-02-02 18:42:22 +0900

    add bubble sort unittest module

--- 4_sort/bubble_sort.py
+++ 4_sort/bubble_sort.py
@@ -1,13 +1,13 @@
 def bubble_sort(sorted_list):
     length = len(sorted_list) -1
     for i in range(length):
+        swapped = False
         for j in range(length -i):
             if sorted_list[j] > sorted_list[j+1]:
                 sorted_list[j], sorted_list[j+1] = sorted_list[j+1], sorted_list[j]
+                swapped = True
+        if not swapped:
+            break   # if no swap happened in the inner loop, the list is already sorted
     return sorted_list
 
-list = [64, 34, 25, 12, 22, 11, 90]
-print('length = ', len(list))
-print(bubble_sort(list))
-
-        
+       
