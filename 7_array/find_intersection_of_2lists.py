bbfc5286b03b0e1478f3622a9fcebef251c9f359
Tae-ho Kim <HoTaeWang@gmail.com>
2025-04-02 23:37:27 +0900

    find intersection example using set.intersection function

--- 7_array/find_intersection_of_2lists.py
+++ 7_array/find_intersection_of_2lists.py
@@ -1,12 +1,20 @@
 this_weeks_winners = [2, 43, 48, 62, 64, 28, 3, 41]
 most_common_winners = [1, 28, 42, 70, 2, 10, 62, 31, 4, 14]
 
+# Method 1: Using list comprehension    
 def return_intersection(list1, list2):
     intersection = [v for v in list1 if v in list2]
     return intersection
 
+# Method 2: Using set intersection
+def return_interset(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    intersection = list(set1.intersection(set2))
+    return intersection
 
 list1 = [2, 43, 48, 62, 64, 28, 3]
 list2 = [1, 28, 42, 70, 2, 10, 62, 31, 4, 14]
 print("intersection = ", return_intersection(list1, list2))
 # Output: [2, 28, 62]
+print("interset =", return_interset(list1, list2))
